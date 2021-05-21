8ef07587259e15c26ab3366a1ecd4b42a11bc961
puraz <leonback34@gmail.com>
2021-05-21 12:13:40 +0300

    Changed Classes #1.2

--- src/me/puraz/sqlite/Main.java
+++ src/me/puraz/sqlite/Main.java
@@ -1,9 +1,14 @@
 package me.puraz.sqlite;
 
+import me.puraz.sqlite.connector.SQLiteConnector;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Test.");
+        SQLiteConnector.connectdb("db.db");
+        SQLiteConnector.createTable("db.db");
+        SQLiteConnector.readTable("db.db");
     }
 
 }
